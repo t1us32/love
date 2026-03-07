e1dc4be8906a29945c3b852cc1012514a7dcd0eb
unknown <t1us3222@@gmail.com>
2026-03-07 22:39:10 +0200

    Fix Render build: make SSL conditional

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { readFileSync } from 'fs'
+import { readFileSync, existsSync } from 'fs'
+
+const httpsConfig = existsSync('./key.pem') && existsSync('./cert.pem')
+  ? {
+    key: readFileSync('./key.pem'),
+    cert: readFileSync('./cert.pem'),
+  }
+  : false
 
 // https://vite.js.org/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    https: {
-      key: readFileSync('./key.pem'),
-      cert: readFileSync('./cert.pem'),
-    }
+    https: httpsConfig
   }
 })
